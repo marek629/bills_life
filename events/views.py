71801ab0f11109787c94452316fef1bfdd51864d
Marek Jędryka <marek629@poczta.onet.pl>
2015-04-22 00:42:31 +0200

    Stub view for events module was added.

--- events/views.py
+++ events/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from events.models import Event
+
+
+def index(request, arg2):
+    event_list = Event.objects.order_by('date')[:5]
+    context = {'event_list': event_list}
+    return render(request, 'events/index.html', context)
